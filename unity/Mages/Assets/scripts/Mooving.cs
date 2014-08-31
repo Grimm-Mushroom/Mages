8b734461d2721e1419a150b5ac490b768f6a851a
erastofen <realmknight@yandex.ru>
2014-08-31 23:15:34 +0400

    по заданным вершинам
    шарик катается по заданному массивы маяков-платформ

--- unity/Mages/Assets/scripts/Mooving.cs
+++ unity/Mages/Assets/scripts/Mooving.cs
@@ -3,17 +3,35 @@ using System.Collections;
 
 public class Mooving : MonoBehaviour {
 	private NavMeshAgent _agent;
-	private Vector3 _target;
-	private bool _b_target = false;
+	//private Vector3 _target;
+	//private bool _b_target = false;
+	private int _step_count = 0;
+
+	public GameObject[] buildings;
+
+
+
 
 	// Use this for initialization
 	void Start () {
 		_agent = gameObject.GetComponent<NavMeshAgent> ();	
+		//_buildings = GameObject.FindGameObjectsWithTag ("Building");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
+		if (Vector3.Distance (_agent.destination, transform.position) < 1.5f) {
+			_step_count++;
+			if (_step_count == 6) {
+				_step_count = 0;
+			}
+			_agent.SetDestination(buildings[_step_count].transform.position);
+		}
+
+		/*if (Input.GetMouseButtonDown (0)) {
+			foreach(GameObject building in buildings) {
+				Debug.Log(building.collider.bounds.size);
+			}
 			Ray _ray;
 			RaycastHit _hit;
 
@@ -21,15 +39,10 @@ public class Mooving : MonoBehaviour {
 			if(Physics.Raycast(_ray, out _hit, 1000.0f)) {
 				_target = _hit.point;
 				_b_target = true;
-			}
-		}
-
-		if (_b_target) {
-			if(Vector3.Distance(_target, transform.position) > 1.5f) {
 				_agent.SetDestination(_target);
-			} else {
-				_agent.Stop();
 			}
-		}
+		}*/
+
+
 	}
 }
