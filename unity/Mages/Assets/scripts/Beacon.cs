35aec35e060369559036b60a08ebbeeb7fdbc7b9
erastofen <realmknight@yandex.ru>
2014-09-10 22:53:14 +0400

    группы маяков и платформ
    группы маяков и платформ
    наличие хозяина
    смена хозяина
    следующий уровень

---
+++ unity/Mages/Assets/scripts/Beacon.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beacon : MonoBehaviour {
+
+	public int owner;
+	public Platform[] platforms; 
+
+	 
+	private void __changeOwner(int own) {
+		owner = own;
+		__changeOwnerAnim (own);
+		foreach(Platform platform in platforms) {
+			platform.changeOwner(own);
+		}
+	}
+
+	private void __changeOwnerAnim(int own) {
+		if (owner < 0) {
+			renderer.material.color = new Color(0, 0, 0);
+		} else if (owner > 0) {
+			renderer.material.color = new Color(1, 1, 1);
+		}
+	}
+
+	private void __checkEnd() {
+		if ((tag == "Finish Beacon") && (owner == 1)) {
+			if (Application.loadedLevel + 1 != Application.levelCount)
+				Application.LoadLevel(Application.loadedLevel + 1);
+			else
+				Application.LoadLevel(0);
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		__changeOwner (owner);
+		Debug.Log (tag);
+			//_buildings = GameObject.FindGameObjectsWithTag ("Building");
+	}
+
+
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {		
+			Ray _ray;
+			RaycastHit _hit;
+			_ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(_ray, out _hit, 1000.0f)) {
+				if (_hit.collider.gameObject == gameObject) {
+					if(owner == 0) {
+						__changeOwner(1);
+					} else {
+						__changeOwner(owner * -1);
+					}
+					__checkEnd();
+				}
+			}
+		}
+		
+	}
+}
---
+++ unity/Mages/Assets/scripts/Platform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	public int owner;
+	public Beacon MyBeacon;
+
+	public void changeOwner(int own) {
+		owner = own;
+		__changeOwnerAnim (); 
+	}
+
+	private void __changeOwnerAnim() {
+		renderer.material.color = MyBeacon.renderer.material.color;
+	}
+
+
+
+
+}
