cfae56283b13129b37debe8c0b07d97011492b07
Yauhen_Sedliarou <Yauhen_Sedliarou@epam.com>
2014-09-14 20:09:03 +0300

    player and resources generation + gui

---
+++ unity/Mages/Assets/scripts/GuiData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiData : MonoBehaviour {
+
+	public Player player;
+
+	public int resources;
+
+	void Start () {
+		InvokeRepeating("__process", 5.0f, 5.0f);
+	}
+	
+	void Update () {
+	
+	}
+
+	private void __process() {
+		resources += player.beacons.Count;
+	}
+
+	void OnGUI () {
+		GUI.Label (new Rect (10, 10, 100, 20), resources.ToString());
+	}
+
+}
---
+++ unity/Mages/Assets/scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public Color color;
+
+	public IList<Beacon> beacons = new List<Beacon>();
+
+	void Start () {
+
+	}
+	
+	void Update () {
+	
+	}
+
+}
