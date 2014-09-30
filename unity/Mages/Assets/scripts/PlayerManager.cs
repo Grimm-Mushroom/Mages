17e27907266a81801061ad82ad71f0f68f96e9aa
ysedliarou <yauhen.sedliarou@gmail.com>
2014-09-30 23:13:57 +0300

    spells

---
+++ unity/Mages/Assets/scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	private static PlayerManager _instance;
+	
+	public static PlayerManager Instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<PlayerManager>();;
+			}
+			return _instance;
+		} 
+	}
+
+	public Player player, enemy, nobody;
+
+}
