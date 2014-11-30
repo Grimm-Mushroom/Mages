84b70cc88753c73516f52f91925b28b1af076759
eRASTOFEN <realmknight@yandex.ru>
2014-11-30 22:48:03 +0300

    hp

---
+++ unity/Mages/Assets/scripts/Creature/HealthPoint.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPoint {
+
+
+
+	public bool status = true;
+
+	public HealthPoint(float healthValue) {
+		health = healthValue;
+	}
+
+	private IEffect __effect = null ;
+	public IEffect effect {
+		get {
+			return __effect;
+		}
+		set {
+			if (__effect == null) {
+				__effect = value;
+			} 
+		}
+	}
+
+	private float __health;
+	public float health {
+		get {
+			return __health;
+		}
+		set {
+			if (value <= 0) {
+				status = false;
+			}
+			__health = value;
+		} 
+	}
+
+}
